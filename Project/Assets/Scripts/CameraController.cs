1b62170c370b6a2e25c7f59411d465c4c1e0f09e
GhostKnights <justcu@163.com>
2020-03-22 11:32:21 +0800

    add

---
+++ Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Vector3 offset;
+    public GameObject person;
+
+    void Start()
+    {
+       
+        person = GameObject.Find("blueobj");
+
+        offset = person.transform.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*this.transform.position = person.transform.position - offset;
+        Quaternion angel = Quaternion.LookRotation(person.transform.position - this.transform.position);
+        this.transform.rotation = angel;*/
+        this.transform.LookAt(person.transform.position);
+    }
+}
